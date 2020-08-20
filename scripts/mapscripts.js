94efca1627cc154ef7d8f5e3f24dd66f236e916f
Austin Wayman <awayman@lavidge.com>
2020-08-20 11:09:50 -0700

    change icon color on map

--- scripts/mapscripts.js
+++ scripts/mapscripts.js
@@ -9,6 +9,25 @@ var xy = function(x, y) {
     }
     return yx(y, x);  // When doing xy(x, y);
 };
+
+
+var redIcon = L.icon({
+    iconUrl: './images/marker-icon-red.png',
+    // shadowUrl: 'leaf-shadow.png',
+
+    iconSize:     [25, 40], // size of the icon
+    // shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [15, 40], // point of the icon which will correspond to marker's location
+    // shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -40] // point from which the popup should open relative to the iconAnchor
+});
+
+
+
+
+
+
+// LOCATIONS
 var baggister = xy(730.0, 165.0);
 var valtorek  = xy(795.0, 285.0);
 var beachfort = xy(745.0, 405.0);
@@ -20,16 +39,16 @@ var brinhd    = xy(605.0, 605.0);
 var frostgate = xy(555.0, 642.0);
 var sanctuaryofeternallight = xy(530.0, 710.0);
 
-L.marker(baggister).addTo(mymap).bindPopup('Baggister');
-L.marker( valtorek).addTo(mymap).bindPopup('Val Torek');
-L.marker(beachfort).addTo(mymap).bindPopup('Beachfort');
-L.marker(     lise).addTo(mymap).bindPopup(     'Lise');
-L.marker(   fallia).addTo(mymap).bindPopup(   'Fallia');
-L.marker(    vaela).addTo(mymap).bindPopup(    'Vaela');
-L.marker(    sunia).addTo(mymap).bindPopup(    'Sunia');
-L.marker(   brinhd).addTo(mymap).bindPopup(   'Brinhd');
-L.marker(frostgate).addTo(mymap).bindPopup('Frostgate');
-L.marker(sanctuaryofeternallight).addTo(mymap).bindPopup('Sanctuary of Eternal Light');
+L.marker(baggister, {icon: redIcon}).addTo(mymap).bindPopup('Baggister');
+L.marker( valtorek, {icon: redIcon}).addTo(mymap).bindPopup('Val Torek');
+L.marker(beachfort, {icon: redIcon}).addTo(mymap).bindPopup('Beachfort');
+L.marker(     lise, {icon: redIcon}).addTo(mymap).bindPopup(     'Lise');
+L.marker(   fallia, {icon: redIcon}).addTo(mymap).bindPopup(   'Fallia');
+L.marker(    vaela, {icon: redIcon}).addTo(mymap).bindPopup(    'Vaela');
+L.marker(    sunia, {icon: redIcon}).addTo(mymap).bindPopup(    'Sunia');
+L.marker(   brinhd, {icon: redIcon}).addTo(mymap).bindPopup(   'Brinhd');
+L.marker(frostgate, {icon: redIcon}).addTo(mymap).bindPopup('Frostgate');
+L.marker(sanctuaryofeternallight, {icon: redIcon}).addTo(mymap).bindPopup('Sanctuary of Eternal Light');
 
 // var travel = L.polyline([sol, deneb]).addTo(mymap);
 
